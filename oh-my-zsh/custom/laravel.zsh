79d4359c2f08d6fac04e33a3db6437ec53e175b8
Matthew Davis <matt@mattdavis.co.uk>
2018-08-22 09:59:05 +0100

    Add Laravel log tailing alias

--- oh-my-zsh/custom/laravel.zsh
+++ oh-my-zsh/custom/laravel.zsh
@@ -3,3 +3,4 @@ alias a="php artisan"
 alias art="php artisan"
 alias tinker="php artisan tinker"
 
+alias llog='tail -f -n 450 storage/logs/laravel*.log | grep -i -E "^\[\d{4}\-\d{2}\-\d{2} \d{2}:\d{2}:\d{2}\]|Next [\w\W]+?\:" --color'
