17e506f3e4efba17f4e5f9c570808d3d3b1a63cc
Matthew Davis <matt@mattdavis.co.uk>
2020-07-23 14:09:15 +0100

    Add some git aliases

--- oh-my-zsh/custom/git.zsh
+++ oh-my-zsh/custom/git.zsh
@@ -1,2 +1,6 @@
 alias nah="git reset --hard; git clean -df"
 alias prune="git remote prune origin"
+alias wip="git add . && git commit -m 'WIP'"
+alias stage-it="git co staging && git merge master && git push origin staging && git co master"
+alias release-it="git co release && git merge staging && git push origin release && git co master"
+alias stage-and-release-it="git co staging && git merge master && git push origin staging && git co release && git merge staging && git push origin release && git co master"
